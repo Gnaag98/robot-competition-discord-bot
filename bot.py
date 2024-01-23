719841b3054356535a0cbd12963a93550c9bda1d
Henrik Wahlström <henrik.g.wahlstrom@gmail.com>
2024-01-23 16:04:07 +0100

    Retry connection until successful
    Retry connection until connection is successful. To make this work the client logic was converted from decorators to a custom class.

--- bot.py
+++ bot.py
@@ -1,98 +1,46 @@
 import os
 
-import discord
-from discord import Member
-
+import asyncio
+from aiohttp import ClientConnectorError
+from discord import Intents, utils
 from dotenv import load_dotenv
+
+from house_robot import HouseRobot, RoleAffixes
 
 load_dotenv()
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD_ID = int(os.getenv('GUILD_ID'))
 
-YEAR_ROLE_PREFIX = os.getenv('YEAR_ROLE_PREFIX')
-BADGE_ROLE_PREFIX = os.getenv('BADGE_ROLE_PREFIX')
-BADGE_ROLE_SUFFIX = os.getenv('BADGE_ROLE_SUFFIX')
+ROLE_AFFIXES = RoleAffixes(
+    year_prefix=os.getenv('YEAR_ROLE_PREFIX'),
+    badge_prefix=os.getenv('BADGE_ROLE_PREFIX'),
+    badge_suffix=os.getenv('BADGE_ROLE_SUFFIX'))
 
-intents = discord.Intents.default()
+intents = Intents.default()
 intents.members = True
 
-client = discord.Client(intents=intents)
+utils.setup_logging(root=False)
 
 
-async def adjust_badge_roles(member: Member):
-    badge_roles = [
-        role for role in member.guild.roles
-        if role.name.startswith(BADGE_ROLE_PREFIX)
-        and role.name.endswith(BADGE_ROLE_SUFFIX)]
+async def main():
+    wait_time_seconds = 10
 
-    
-    # Add None to the list of badge roles so that the index correspond to the
-    # number of years participated. This works because the roles are retrieved
-    # from lowest role to highest.
-    badge_roles = (
-        None,
-        *badge_roles
-    )
-    
-    current_year_roles = [
-        role for role in member.roles
-        if role.name.startswith(YEAR_ROLE_PREFIX)]
-
-    # Clamp the badge index to the valid range [0, len(badge_roles))
-    badge_role_index = min(len(current_year_roles), len(badge_roles))
-
-    # Choose the correct role based on the number of years participated.
-    correct_badge_role = badge_roles[badge_role_index]
-
-    current_badge_roles = [
-        role for role in member.roles
-        if role.name.startswith(BADGE_ROLE_PREFIX)]
-
-    roles_to_remove = [
-        role for role in current_badge_roles
-        if role and role != correct_badge_role]
-
-    # Add and remove roles as necessary.
-    if correct_badge_role and not correct_badge_role in current_badge_roles:
-        print(f'Adding role {correct_badge_role.name} to {member.name}')
-        await member.add_roles(correct_badge_role)
-    if roles_to_remove:
-        print(f'Removing roles {", ".join(role.name for role in roles_to_remove)} from {member.name}')
-        await member.remove_roles(*roles_to_remove)
-
-
-@client.event
-async def on_ready():
-    print(f'{client.user} has connected to Discord!')
-
-    guild = client.get_guild(GUILD_ID)
-
-    print('Adjusting roles...')
-    for member in guild.members:
-        await adjust_badge_roles(member)
-    
-    print('Done adjusting roles.')
-
-
-@client.event
-async def on_member_update(before: Member, after: Member):
-    if before.roles != after.roles:
-        year_roles_before = [
-            role for role in before.roles
-            if role.name.startswith(YEAR_ROLE_PREFIX)]
-        year_roles_after = [
-            role for role in after.roles
-            if role.name.startswith(YEAR_ROLE_PREFIX)]
-        
-        # Check if the list contains the same roles. This works because the
-        # roles appear in the same order.
-        if year_roles_before != year_roles_after:
-            await adjust_badge_roles(after)
-
-
-def main():
-    client.run(DISCORD_TOKEN)
+    while True:
+        try:
+            # XXX: Recreates client since I don't know how to reuse it.
+            client = HouseRobot(GUILD_ID, ROLE_AFFIXES, intents=intents)
+            await client.start(DISCORD_TOKEN)
+            break
+        except ClientConnectorError:
+            print('Connection failed.')
+            # XXX: Closes the client since it is recreated each time.
+            await client.close()
+            print(f'Retrying in { wait_time_seconds } seconds...')
+            await asyncio.sleep(wait_time_seconds)
 
 
 if __name__ == '__main__':
-    main()
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        pass
---
+++ house_robot.py
@@ -0,0 +1,89 @@
+from attr import dataclass
+
+from discord import Client, Member
+
+
+@dataclass
+class RoleAffixes:
+    """Prefixes and suffixes for the roles."""
+    year_prefix: str
+    badge_prefix: str
+    badge_suffix: str
+
+
+class HouseRobot(Client):
+    """Custom client."""
+
+    def __init__(self, guild_id: int, affixes: RoleAffixes, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        
+        self.guild_id = guild_id
+        self.role_affixes = affixes
+    
+
+    async def on_ready(self):
+        print(f'{self.user} has connected to Discord!')
+
+        guild = self.get_guild(self.guild_id)
+
+        print('Adjusting roles...')
+        for member in guild.members:
+            await self.adjust_badge_roles(member)
+        
+        print('Done adjusting roles.')
+
+
+    async def on_member_update(self, before: Member, after: Member):
+        if before.roles != after.roles:
+            year_roles_before = [
+                role for role in before.roles
+                if role.name.startswith(self.role_affixes.year_prefix)]
+            year_roles_after = [
+                role for role in after.roles
+                if role.name.startswith(self.role_affixes.year_prefix)]
+            
+            # Check if the list contains the same roles. This works because the
+            # roles appear in the same order.
+            if year_roles_before != year_roles_after:
+                await self.adjust_badge_roles(after)
+    
+
+    async def adjust_badge_roles(self, member: Member):
+        badge_roles = [
+            role for role in member.guild.roles
+            if role.name.startswith(self.role_affixes.badge_prefix)
+            and role.name.endswith(self.role_affixes.badge_suffix)]
+
+        # Add None to the list of badge roles so that the index correspond to the
+        # number of years participated. This works because the roles are retrieved
+        # from lowest role to highest.
+        badge_roles = (
+            None,
+            *badge_roles
+        )
+        
+        current_year_roles = [
+            role for role in member.roles
+            if role.name.startswith(self.role_affixes.year_prefix)]
+
+        # Clamp the badge index to the valid range [0, len(badge_roles))
+        badge_role_index = min(len(current_year_roles), len(badge_roles))
+
+        # Choose the correct role based on the number of years participated.
+        correct_badge_role = badge_roles[badge_role_index]
+
+        current_badge_roles = [
+            role for role in member.roles
+            if role.name.startswith(self.role_affixes.badge_prefix)]
+
+        roles_to_remove = [
+            role for role in current_badge_roles
+            if role and role != correct_badge_role]
+
+        # Add and remove roles as necessary.
+        if correct_badge_role and not correct_badge_role in current_badge_roles:
+            print(f'Adding role {correct_badge_role.name} to {member.name}')
+            await member.add_roles(correct_badge_role)
+        if roles_to_remove:
+            print(f'Removing roles {", ".join(role.name for role in roles_to_remove)} from {member.name}')
+            await member.remove_roles(*roles_to_remove)
