122d45348a225a73898b55f368ad149bfbc8cd3f
Henrik Wahlström <henrik.g.wahlstrom@gmail.com>
2025-07-03 09:51:01 +0200

    Update comments
    Updated comments in main.py and renamed a variable.

--- main.py
+++ main.py
@@ -6,34 +6,43 @@ from discord import Intents, utils
 
 from house_robot import HouseRobot
 
+# Choose which events to listen for. Some need to be enabled in the developer
+# portal as well.
 intents = Intents.default()
 intents.members = True
 intents.message_content = True
 
+# Enable logging
 utils.setup_logging(root=False)
 
 
 async def main():
-    wait_time_seconds = 10
+    connection_retry_delay = 10 # seconds
 
+    # Keep the client connected. Only exits if the client stops without error.
     while True:
         try:
+            # Load data from files.
             with open('settings.json', encoding='utf-8') as json_file:
                 settings = json.load(json_file)
             with open('responses.json', encoding='utf-8') as json_file:
                 doorbell_responses = json.load(json_file)
 
-            # XXX: Recreate client since I don't know how to reuse it.
+            # XXX: Recreate client each try. Could probably be reused instead.
             client = HouseRobot(intents=intents, settings=settings,
                                 doorbell_responses=doorbell_responses)
+            # Short for login() + connect().
             await client.start(settings['discord_token'])
+            # Exit if client stopped without error.
             break
+        # XXX: This is an asyncio exception. Maybe Discord.py exceptions should
+        # be caught instead.
         except ClientConnectorError:
             print('Connection failed.')
             # XXX: Close the client since it is recreated each time.
             await client.close()
-            print(f'Retrying in { wait_time_seconds } seconds...')
-            await asyncio.sleep(wait_time_seconds)
+            print(f'Retrying in { connection_retry_delay } seconds...')
+            await asyncio.sleep(connection_retry_delay)
 
 
 if __name__ == '__main__':
